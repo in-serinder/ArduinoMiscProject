87406f0e6b626eefcefc2102056ac2359f35f859
Argovict <serinder@163.com>
2025-08-07 06:08:40 +0800

    Update

--- WS_Servo/WS_Servo.ino
+++ WS_Servo/WS_Servo.ino
@@ -1,15 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
+#include <Servo.h>
 
+
+#define ServoPWM D4
 
 const char *WIFI_SSID = "2.4GHZ";
-const char *WIFI_PASSWORD = "password";
+const char *WIFI_PASSWORD = "320724fuck";
 const uint8_t WS_PORT = 81;
+const uint16_t SERVO_ANGLE = 180;
 
 
 
 WiFiClient wc;
 WebSocketsServer ws = WebSocketsServer(WS_PORT);  //指定对象端口
+Servo servo;
 
 void Wifi_connect() {
   int count = 0;
@@ -26,10 +31,8 @@ void Wifi_connect() {
   Serial.println("\nMAC: " + WiFi.macAddress() + "\nIP: " + WiFi.localIP().toString() + "\nUsing Time:" + count + "s");
 }
 
-// 绑定事件函数
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
   Serial.printf("Event Type : [%d]\n", type);
-  // 事件处理
   switch (type) {
     case WStype_CONNECTED:
       Serial.println("Ws Connected\n");
@@ -44,7 +47,15 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
       Serial.printf("Got binary length:[%d]\n", length);
       break;
     case WStype_TEXT:
-      Serial.printf("Got a Text Message [%s]\n", payload);
+      {
+        Serial.printf("Got a Text Message [%s]\n", payload);
+        // 直接使用文本
+        // int Angle = (atoi(payload) == SERVO_ANGLE) ? SERVO_ANGLE : (atoi(payload) % SERVO_ANGLE);
+        int Angle = (atoi((const char *)payload) % SERVO_ANGLE + 1) - 1;
+        Serial.printf("Servo [%d]\n", Angle);
+        servo.write(Angle);
+        // servo.writeMicroseconds(2700);
+      }
       break;
     case WStype_PING:
       break;
@@ -59,6 +70,7 @@ void setup() {
   // put your setup code here, to run once:
   // init
   Serial.begin(9600);
+  servo.attach(ServoPWM);
   Wifi_connect();
   ws.begin();
   ws.onEvent(webSocketEvent);
